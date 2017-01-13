ac9a5811372baca772156a31958d19a29a23a57c
Antonio Molinari <molinari@incode.it>
2017-01-13 23:56:54 +0100

    updates .vimrc .gitignore

--- bin/startInsecureChrome.sh
+++ bin/startInsecureChrome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' --allow-running-insecure-content --disable-web-security
+'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' --allow-running-insecure-content --disable-web-security --user-data-dir
 
